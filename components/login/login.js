3144b5ba1af0812c25b07ce4ac5b131a57519d32
venubabuperam <venubabu119@gmail.com>
2025-01-17 04:19:07 +0530

    schedule commit

--- components/login/login.js
+++ components/login/login.js
@@ -1,29 +1,28 @@
 app.controller('LoginController', function ($scope, $http, $location) {
-    console.log('hello');
-    $scope.login = function (credentials) {
-      $http.get('http://localhost:3000/users').then(
-        function (response) {
-          const users = response.data;
-          console.log(users);
-          const user = users.find(
-            u =>
-              u.email === credentials.email &&
-              u.password === credentials.password
-          );
-          console.log(user);
-  
-          if (user) {
-            alert('Login successful!');
-            // Redirect based on user type or other conditions
-            $location.path('/home'); // Adjust the path as needed
-          } else {
-            alert('Invalid email or password.');
-          }
-        },
-        function (error) {
-          console.error('Error:', error);
+  console.log('hello');
+  $scope.login = function (credentials) {
+    $http.get('http://localhost:3000/users').then(
+      function (response) {
+        const users = response.data;
+        console.log(users);
+        const user = users.find(
+          u =>
+            u.email === credentials.email &&
+            u.password === credentials.password
+        );
+        console.log(user);
+
+        if (user) {
+          alert('Login successful!');
+          // Redirect based on user type or other conditions
+          $location.path('/home'); // Adjust the path as needed
+        } else {
+          alert('Invalid email or password.');
         }
-      );
-    };
-  });
-  
+      },
+      function (error) {
+        console.error('Error:', error);
+      }
+    );
+  };
+});
